87fbe9f8de13ea1dc2ce46e3789629da0f82a648
annemariv <annemari.velling@gmail.com>
2025-10-14 14:22:27 +0300

    Fix for Model and adding package

--- ToDoApp2/Models/Task.cs
+++ ToDoApp2/Models/Task.cs
@@ -10,7 +10,7 @@ namespace ToDoApp2.Models
     {
         public int Id { get; set; }
 
-        public string Title { get; set; } = string.Empty;
+        public string Title { get; set; }
 
         public DateTime? DueDate { get; set; }
 
