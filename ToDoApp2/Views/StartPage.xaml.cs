76dfa2dfd4000196e5fe10c722ba6816e50ac5ef
KerttuRungi <kerttu.rungi@gmail.com>
2025-10-20 11:46:16 +0300

    StartPage in progress
    StartPage currently works but design is in progress

--- ToDoApp2/Views/StartPage.xaml.cs
+++ ToDoApp2/Views/StartPage.xaml.cs
@@ -2,8 +2,13 @@ namespace ToDoApp2.Views;
 
 public partial class StartPage : ContentPage
 {
-	public StartPage()
-	{
-		InitializeComponent();
-	}
+    public StartPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnGetStartedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new TasksPage());
+    }
 }
